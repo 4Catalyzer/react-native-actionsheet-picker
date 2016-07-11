7e65e793c6aea9e8088bdbdbdaf42e881d9839c4
Chirag Jain <jain_chirag04@yahoo.com>
2016-07-11 13:20:29 -0400

    make the api close to actionsheetpicker-3.0

--- CJActionSheetPicker/CJActionSheetPicker/CJActionSheetPicker.h
+++ CJActionSheetPicker/CJActionSheetPicker/CJActionSheetPicker.h
@@ -1,11 +1,3 @@
-//
-//  CJActionSheetPicker.h
-//  CJActionSheetPicker
-//
-//  Created by Chirag Jain on 5/23/16.
-//  Copyright © 2016 Chirag Jain. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "RCTBridge.h"
