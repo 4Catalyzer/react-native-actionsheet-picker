da1a2cd23c08975fa15295d9870cf2dcbefb2d33
Chirag <jain_chirag04@yahoo.com>
2017-10-13 16:59:03 -0400

    support tintColor and fix for selectedIndex -1 (#7)

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import ReactNative from 'react-native';
 
 const {
-  NativeModules: {
-    CJActionSheetPicker,
-  }
+  NativeModules: { CJActionSheetPicker },
+  processColor,
 } = ReactNative;
 
-const {
-  showCountDownPicker,
-  showStringPicker,
-} = CJActionSheetPicker;
+
+async function showCountDownPicker({ tintColor, ...options }) {
+  return await CJActionSheetPicker.showCountDownPicker({
+    tintColor: processColor(tintColor),
+    ...options,
+  });
+}
+
+async function showStringPicker({ tintColor, selectedIndex, ...options }) {
+  return await CJActionSheetPicker.showStringPicker({
+    tintColor: processColor(tintColor),
+    selectedIndex: selectedIndex === -1 ? 0 : selectedIndex,
+    ...options,
+  });
+}
 
 export { showCountDownPicker, showStringPicker };
